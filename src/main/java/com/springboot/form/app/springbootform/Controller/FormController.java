6140b6083721793ce875c1a491f8c517f2358e57
mariana correa <correapintoluzmariana@gmail.com>
2023-07-19 08:35:14 -0500

    automatizacion, cambio de msj de erros

--- src/main/java/com/springboot/form/app/springbootform/Controller/FormController.java
+++ src/main/java/com/springboot/form/app/springbootform/Controller/FormController.java
@@ -32,12 +32,12 @@ public class FormController {
 
         model.addAttribute("titulo", "Resultado form");
         if(result.hasErrors()){
-            Map<String, String> errores = new HashMap<>();
+            /*Map<String, String> errores = new HashMap<>();
             //poblar la lista
             result.getFieldErrors().forEach(err ->{
                 errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
             });
-            model.addAttribute("error", errores);
+            model.addAttribute("error", errores);*/
             return "form";
         }
        
